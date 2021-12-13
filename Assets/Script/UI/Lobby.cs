08ae21dc0243ed488d824ea56f27c3410552bf6c
Killian <32911550+Meshanda@users.noreply.github.com>
2021-12-14 00:25:28 +0100

    UI enhances

--- Assets/Script/UI/Lobby.cs
+++ Assets/Script/UI/Lobby.cs
@@ -8,11 +8,42 @@ using UnityEngine;
 public class Lobby : MonoBehaviour
 {
     [SerializeField] private PlayerNames playerNames;
-
+    [SerializeField] private TextMeshProUGUI subtitleText;
     [SerializeField] private TextMeshProUGUI listText;
-    
+
+    private int _myInt = 0;
+
+    private void Update()
+    {
+        UpdateList();
+    }
+
     public void UpdateList()
     {
-        listText.text += playerNames.Names.Last() + "\n";
+        listText.text += playerNames.Names.Last() + TextLayout();
+        subtitleText.text = playerNames.Names.Count + " players connected";
+    }
+
+    private string TextLayout()
+    {
+        string myString = "";
+
+        switch (_myInt)
+        {
+            case 0:
+                myString = "\t\t\t";
+                break;
+            case 1:
+                myString = "\t\t\t";
+                break;
+            case 2:
+                myString = "\n";
+                break;
+        }
+
+        _myInt += 1;
+        if (_myInt >= 3) _myInt = 0;
+        
+        return myString;
     }
 }
